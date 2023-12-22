5a9f8d8b86db8e8fa705bfd95047a7e942afa382
Bartosz Snieg <snieg45@gmail.com>
2023-12-22 21:44:23 +0100

    Added example app and update submodule

--- apps/example/main.cc
+++ apps/example/main.cc
@@ -1,6 +1,8 @@
-#include <iostream>
-int main(int argc, char const *argv[])
-{
-    std::cout << "Example app" << std::endl;
-    return 0;
+#include "apps/example/router.h"
+#include "core/application/application_factory.h"
+#include "untar/untar.h"
+
+int main(int argc, char const *argv[]) {
+  simba::core::ApplicationFactory<simba::router::Router>::Start(argc, argv);
+  return 0;
 }
