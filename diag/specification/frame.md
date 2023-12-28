5bc5031e6d588be932473fc48611b24e145f93ac
Bartosz Śnieg <snieg45@gmail.com>
2023-12-28 22:11:11 +0100

    Added diag structure (#6)

---
+++ diag/specification/frame.md
@@ -0,0 +1,8 @@
+# Specyfikacja ramki diagnostycznej
+
+| Nazwa | rozmiar | Opis |
+|-------|---------|------|
+| Service_id | 2 | Numer identyfikacyjny serwisu |
+| diag_id | 1 | Numer identyfikacyjny metody diagnostycznej |
+| length | 1 | Długość pola "payload" |
+payload | 0-255 | dodatkowe dane |
---
+++ diag/tests/test_data_structure.cc
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include "diag/data/data_structure.h"
+
+TEST(DATA_STRUCTURE, CONSTRUCTOR_CHECK) {
+  const std::vector<uint8_t> elements{0x10, 0x11, 0x12, 0x13};
+  simba::diag::data::DataStructure sut{0x1111, 0x01};
+  EXPECT_EQ(0x1111, sut.GetServiceID());
+  EXPECT_EQ(0x01, sut.GetDiagID());
+  sut.SetPayload(elements);
+  EXPECT_EQ(0x1111, sut.GetServiceID());
+  EXPECT_EQ(0x01, sut.GetDiagID());
+  EXPECT_EQ(0x1111, sut.GetServiceID());
+  EXPECT_EQ(elements.size(), sut.GetPayload().size());
+  for (uint8_t i = 0; i < sut.GetPayload().size(); i++) {
+    EXPECT_EQ(elements[i], sut.GetPayload()[i]);
+  }
+}
