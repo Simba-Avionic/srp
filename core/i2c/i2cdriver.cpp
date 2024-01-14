5af4b164b92d86d96bdbd7a6769933577dad1f5c
Michal-Mankowski <m.mankowski2004@gmail.com>
2024-01-14 18:25:00 +0100

    poprawiono składnie

--- core/i2c/i2cdriver.cpp
+++ core/i2c/i2cdriver.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file i2cdriver.cpp
+ * @author Michal Mankowski (m.mankowski2004@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-01-14
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include "i2cdriver.h"
 #include <sys/ioctl.h>
 #include <fcntl.h>
@@ -6,7 +16,6 @@
 #include <linux/i2c.h>
 inline __s32 I2C::i2c_smbus_access(int file, char read_write, __u8 command, int size, union i2c_smbus_data *data) {
     struct i2c_smbus_ioctl_data args;
-
     args.read_write = read_write;
     args.command = command;
     args.size = size;
@@ -15,36 +24,32 @@ inline __s32 I2C::i2c_smbus_access(int file, char read_write, __u8 command, int 
 }
 inline __s32 I2C::i2c_smbus_read_byte_data(int file, __u8 command) {
     union i2c_smbus_data data;
-    if (i2c_smbus_access(file, I2C_SMBUS_READ, command,
-                         I2C_SMBUS_BYTE_DATA, &data))
+    if (i2c_smbus_access(file, I2C_SMBUS_READ, command, I2C_SMBUS_BYTE_DATA, &data)) {
         return -1;
-    else
-        return 0xFF & data.byte;
+    }
+    return 0xFF & data.byte;
 }
  inline __s32 I2C::i2c_smbus_write_byte_data(int file, __u8 command, __u8 value) {
     union i2c_smbus_data data;
     data.byte = value;
 
-    if (i2c_smbus_access(file, I2C_SMBUS_WRITE, command,
-                         I2C_SMBUS_BYTE_DATA, &data))
-        return -1;
-    else
-        return 0;
-}
-
-
-int I2C::init(const char * path){
-    this->file = open(path, O_RDWR);
-    if(this->file < 0) {
+    if (i2c_smbus_access(file, I2C_SMBUS_WRITE, command, I2C_SMBUS_BYTE_DATA, &data)) {
         return -1;
     }
-    
     return 0;
-
 }
-uint8_t I2C::read(uint8_t address, uint8_t reg){
-    if(address != this->slaveAddress){
-        if(ioctl(file,I2C_SLAVE,address)<0){
+
+
+int I2C::init(const char * path) {
+    this->file = open(path, O_RDWR);
+    if (this->file < 0) {
+        return -1;
+    }
+    return 0;
+}
+uint8_t I2C::read(uint8_t address, uint8_t reg) {
+    if (address != this->slaveAddress) {
+        if (ioctl(file,I2C_SLAVE,address)<0) {
         return -1;
     }
     this->slaveAddress = address;
@@ -54,12 +59,10 @@ uint8_t I2C::read(uint8_t address, uint8_t reg){
     if (data < 0) {
         return -2;
     }
-
     return data;
-
 }
-int I2C::write(uint8_t address, uint8_t reg, uint8_t data){
-    if(address != this->slaveAddress){
+int I2C::write(uint8_t address, uint8_t reg, uint8_t data) {
+    if (address != this->slaveAddress){
         if(ioctl(file,I2C_SLAVE,address)<0){
         return -1;
     }
--- core/i2c/i2cdriver.h
+++ core/i2c/i2cdriver.h
@@ -1,17 +1,27 @@
-#ifndef I2CDRIVER_H
-#define I2CDRIVER_H
+/**
+ * @file i2cdriver.h
+ * @author Michal Mankowski (m.mankowski2004@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-01-14
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef CORE_I2C_I2CDRIVER_H_
+#define CORE_I2C_I2CDRIVER_H_
 #include <linux/types.h>
 #include <stdint.h>
 class I2C{
     public:
-    int init(const char * path);
-    uint8_t read(uint8_t address, uint8_t reg);
-    int write(uint8_t address, uint8_t reg, uint8_t data);
+        int init(const char * path);
+        uint8_t read(uint8_t address, uint8_t reg);
+        int write(uint8_t address, uint8_t reg, uint8_t data);
     private:
-    int file = -1;
-    uint8_t slaveAddress;
-    static inline __s32 i2c_smbus_access(int file, char read_write, __u8 command, int size, union i2c_smbus_data *data);
-    static inline __s32 i2c_smbus_read_byte_data(int file, __u8 command);
-    static inline __s32 I2C::i2c_smbus_write_byte_data(int file, __u8 command, __u8 value);
+        int file = -1;
+        uint8_t slaveAddress;
+        static inline __s32 i2c_smbus_access(int file, char read_write, __u8 command, int size, union i2c_smbus_data *data);
+        static inline __s32 i2c_smbus_read_byte_data(int file, __u8 command);
+        static inline __s32 I2C::i2c_smbus_write_byte_data(int file, __u8 command, __u8 value);
 };
-#endif
+#endif // CORE_I2C_I2CDRIVER_H_
