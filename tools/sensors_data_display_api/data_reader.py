a0028027dca14b0334e543634b3c38396da73058
ola567 <aleksandra.hein@o2.pl>
2024-03-10 09:55:17 +0100

    remove unnecesary lines

--- tools/sensors_data_display_api/data_reader.py
+++ tools/sensors_data_display_api/data_reader.py
@@ -13,35 +13,3 @@ class DataReader:
             self.data += 1
             self.gui.update_data_label(self.data)
             time.sleep(1)
-
-
-class GUI(tk.Tk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Odczyt danych")
-        self.geometry("200x100")
-
-        self.label = tk.Label(self, text="Dane:")
-        self.label.pack()
-
-        self.data_label = tk.Label(self, text="")
-        self.data_label.pack()
-
-        self.data_reader = DataReader(self)
-        self.start_reading()
-
-    def start_reading(self):
-        self.data_reader_thread = threading.Thread(target=self.data_reader.start_reading)
-        self.data_reader_thread.start()
-
-    def update_data_label(self, data):
-        self.data_label.config(text=str(data))
-
-    def stop_reading(self):
-        self.data_reader.stop_reading()
-        self.data_reader_thread.join()
-
-if __name__ == "__main__":
-    app = GUI()
-    app.mainloop()
