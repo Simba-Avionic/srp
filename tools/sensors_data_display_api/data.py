74f1889b1b7c043c3e508edf7ad2c368cf0a0aa1
ola567 <aleksandra.hein@o2.pl>
2024-03-18 08:05:47 +0100

    changes

--- tools/sensors_data_display_api/data.py
+++ tools/sensors_data_display_api/data.py
@@ -10,6 +10,6 @@ class Data:
         self.vent = None
 
     def is_None(self):
-        if self.temperature_up is None or self.temperature_middle is None or self.temperature_down is None or self.tank_pressure is None:
+        if self.temperature_up is None or self.temperature_middle is None or self.temperature_down is None or self.tank_pressure is None or self.main_valve is None:
             return True
         return False
