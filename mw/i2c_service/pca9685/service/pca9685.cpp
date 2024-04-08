f28247b74960f22c3031aaa7808ac5250b1fc606
Mateusz <matikrajek42@gmail.com>
2024-04-08 21:09:30 +0200

    fix

--- mw/i2c_service/pca9685/service/pca9685.cpp
+++ mw/i2c_service/pca9685/service/pca9685.cpp
@@ -34,21 +34,20 @@ namespace {
  */
 core::ErrorCode PCA9685::SetServo(uint8_t channel, uint16_t pos) {
     std::unique_lock<std::mutex> lock(*this->mtx);
-    this->i2c_->Ioctl(PCA9685_ADDRESS, I2C_SLAVE);
+    this->i2c_.Ioctl(PCA9685_ADDRESS, I2C_SLAVE);
     std::vector<std::pair<uint8_t, uint8_t>> vec = {
         {MODE1_REG, 0x01},  // setup reset mode
         {PRESCALE_REG, 121},  // przeskalowanie dla 50 Hz
-        {LED0_ON_L+4*channel, 0x0},  // ON LOW REG Val
-        {LED0_ON_H+4*channel, 0x0},   // ON HIGH REG Val
+        {LED0_ON_L+4*channel, 0x0 & 0xFF},  // ON LOW REG Val
+        {LED0_ON_H+4*channel, 0x0 >> 8},   // ON HIGH REG Val
         {LED0_OFF_L+4*channel, pos & 0xFF},  // OFF LOW REG Val
         {LED0_OFF_H+4*channel, pos >> 8}};   // OFF HIGH REG Val
-    return this->i2c_->Write(vec);
+    return this->i2c_.Write(vec);
 }
 
 core::ErrorCode PCA9685::Init(uint16_t app_id) {
     this->app_id = app_id;
-    this->gpio_->Init(app_id);
-
+    this->gpio_.Init(app_id);
     this->ReadConfig();
     for (auto chan : this->db_) {
         this->SetServo(chan.second.channel, chan.second.off_pos);
@@ -84,11 +83,11 @@ core::ErrorCode PCA9685::AutoSetServoPos(uint8_t actuator_id, uint8_t position) 
         return core::ErrorCode::kNotDefine;
     }
     if (it->second.position != (position == 0 ? it->second.on_pos : it->second.off_pos)) {
-        this->gpio_->SetPinValue(it->second.mosfet_id, gpio::Value::HIGH);
+        this->gpio_.SetPinValue(it->second.mosfet_id, gpio::Value::HIGH);
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
         this->SetServo(it->second.channel, it->second.position == 1 ? it->second.on_pos : it->second.off_pos);
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
-        this->gpio_->SetPinValue(it->second.mosfet_id, gpio::Value::LOW);
+        this->gpio_.SetPinValue(it->second.mosfet_id, gpio::Value::LOW);
         it->second.position = position;
     }
     return core::ErrorCode::kOk;
@@ -101,21 +100,18 @@ core::ErrorCode PCA9685::ManSetServoPos(uint8_t actuator_id, uint16_t position) 
         return core::ErrorCode::kNotDefine;
     }
     if (it->second.position != position) {
-        this->gpio_->SetPinValue(it->second.mosfet_id, gpio::Value::HIGH);
+        this->gpio_.SetPinValue(it->second.mosfet_id, gpio::Value::HIGH);
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
         this->SetServo(it->second.channel, position);
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
-        this->gpio_->SetPinValue(it->second.mosfet_id, gpio::Value::LOW);
+        this->gpio_.SetPinValue(it->second.mosfet_id, gpio::Value::LOW);
         it->second.position = position;
     }
     return core::ErrorCode::kOk;
 }
-PCA9685::PCA9685(std::mutex *mtx, std::unique_ptr<gpio::GPIOController> gpio,
-        std::unique_ptr<core::I2CDriver> i2c):gpio_(std::move(gpio)), i2c_(std::move(i2c)) {
-    this->mtx = mtx;
-}
 PCA9685::PCA9685(std::mutex *mtx) {
     this->mtx = mtx;
+    this->i2c_.Init();
 }
 
 }  // namespace i2c
