bc497e0e5c896f4d2c2cfcb7537c1938d8689e5c
Mateusz <matikrajek42@gmail.com>
2024-04-10 16:46:15 +0200

    fix 2

--- apps/env_service/env_service.cpp
+++ apps/env_service/env_service.cpp
@@ -35,7 +35,7 @@ core::ErrorCode EnvService::Initialize(
     this->temp1_event = std::make_shared<com::someip::EventSkeleton>("EnvApp/newTempEvent_1");
     this->temp2_event = std::make_shared<com::someip::EventSkeleton>("EnvApp/newTempEvent_2");
     this->temp3_event = std::make_shared<com::someip::EventSkeleton>("EnvApp/newTempEvent_3");
-    this->dtc_temp_error = std::make_shared<diag::dtc::DTCObject>(0x10);
+    this->dtc_temp_error = std::make_shared<diag::dtc::DTCObject>(0x20);
     this->dtc_temp_connection_error_0xB0 = std::make_shared<diag::dtc::DTCObject>(0xB0);
     diag_controller.RegisterDTC(dtc_temp_connection_error_0xB0);
     diag_controller.RegisterDTC(dtc_temp_error);
