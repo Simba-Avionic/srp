0bf0e95757c752e56cbf7936776bcfcfe8f4901e
Bartosz Snieg <snieg45@gmail.com>
2024-04-30 22:59:12 +0200

    Bazel update from 5.4.1 to 7.1.1

--- core/i2c/i2cdriver.hpp
+++ core/i2c/i2cdriver.hpp
@@ -1,44 +1,48 @@
 /**
  * @file i2cdriver.hpp
  * @author Mateusz Krajewski (matikrajek42@gmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-04-03
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #ifndef CORE_I2C_I2CDRIVER_HPP_
 #define CORE_I2C_I2CDRIVER_HPP_
 
-#include <sys/ioctl.h>
 #include <fcntl.h>
 #include <linux/i2c-dev.h>
-#include <linux/types.h>
 #include <linux/i2c.h>
+#include <linux/types.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
-#include <string>
-#include <vector>
-#include <utility>
+
+#include <cstdint>
 #include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
 #include "core/common/error_code.h"
 
 namespace simba {
 namespace core {
 namespace i2c {
 
-class I2CDriver{
+class I2CDriver {
  private:
   int i2cFile;
+
  public:
   core::ErrorCode Init();
   core::ErrorCode Ioctl(const uint8_t address, const uint16_t type = I2C_SLAVE);
   /**
    * @brief Function to write data to device on i2c bus
-   * 
+   *
    * @param data pair ( req, data)
-   * @return core::ErrorCode 
+   * @return core::ErrorCode
    */
   core::ErrorCode Write(const std::vector<uint8_t> RegData);
   core::ErrorCode PageWrite(std::vector<uint8_t> data);
