d7a82af6e55c4eb27dce61459667a4834fa5e0a3
Mateusz <matikrajek42@gmail.com>
2024-05-23 17:10:34 +0200

    example_for_test(to_remove)

--- apps/example/router.cc
+++ apps/example/router.cc
@@ -27,47 +27,20 @@ namespace simba {
 namespace router {
 
 core::ErrorCode Router::Run(std::stop_token token) {
-  uint8_t servo_pos;
-  auto proxy_event = std::make_shared<com::someip::EventProxyBase>(
-      "ExampleApp/someevent",
-      [this](const std::vector<uint8_t>) { AppLogger::Info("EVENT !!!!!!!"); });
-  auto example = std::make_shared<com::someip::MethodSkeleton>(
-      "ExampleApp/exampleMethod",
-      [this](const std::vector<uint8_t> payload)
-          -> std::optional<std::vector<uint8_t>> {
-        return std::vector<uint8_t>{0, 1, 2};
-      });
-  auto off_prime = std::make_shared<com::someip::MethodProxyBase>("ExampleApp/offPrime");
-  auto on_prime = std::make_shared<com::someip::MethodProxyBase>("ExampleApp/onPrime");
-  auto servo  = std::make_shared<com::someip::MethodProxyBase>("ExampleApp/setServoValue");
-  com->Add(example);
-  com->Add(on_prime);
-  com->Add(servo);
-  com->Add(off_prime);
-  com->Add(proxy_event);
-  on_prime->StartFindService();
-  servo->StartFindService();
-  off_prime->StartFindService();
-  proxy_event->StartFindService();
-  auto dtc = std::make_shared<diag::dtc::DTCObject>(0x20);
-  diag_controller.RegisterDTC(dtc);
+
   while (true) {
-    dtc->Pass();
-    std::ignore = on_prime->Get();
-    std::ignore = servo->Get({1});
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::ignore = off_prime->Get();
-    std::ignore = servo->Get({0});
-    dtc->Failed();
+    this->pwm_.AutoSetServoPosition(61, 1);
     std::this_thread::sleep_for(std::chrono::seconds(1));
+    this->pwm_.AutoSetServoPosition(61, 0);
   }
   return core::ErrorCode::kOk;
 }
 
 core::ErrorCode Router::Initialize(
     const std::unordered_map<std::string, std::string>& parms) {
-  this->gpio_ = gpio::GPIOController(new com::soc::IpcSocket());
-  this->gpio_.Init(12);
+  this->gpio_ = gpio::GPIOController();
+  this->pwm_.Init(parms);
   return core::ErrorCode::kOk;
 }
 }  // namespace router
