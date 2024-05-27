2ce831a2bfa6aabc9f065a5538ac62c08f84d508
Michal-Mankowski <m.mankowski2004@gmail.com>
2024-05-27 21:20:42 +0200

    Temp tests done

--- mw/temp/tests/test_subscribe_msg_factory.cpp
+++ mw/temp/tests/test_subscribe_msg_factory.cpp
@@ -1,9 +1,9 @@
 /**
  * @file test_subscribe_msg_factory.cpp
- * @author Maciek Matuszewski (maciej.matuszewsky@gmail.com)
+ * @author Michał Mańkowski (m.mankowski2004@gmail.com)
  * @brief 
  * @version 0.1
- * @date 2024-03-02
+ * @date 2024-05-27
  * 
  * @copyright Copyright (c) 2024
  * 
@@ -13,10 +13,32 @@
 #include "mw/temp/subscribe_msg/subscribe_header.h"
 #include "mw/temp/subscribe_msg/subscribe_msg_factory.h"
 
-TEST(SUBSCRIBE_MSG_FACTORIES, SUBSCRIBE_MSG_FACTORIES_TEST) {
+class SubscribeMsgFactoriesTest : public ::testing::TestWithParam<
+                        std::tuple<std::vector<uint8_t>, uint16_t>> {
+};
+
+INSTANTIATE_TEST_SUITE_P(SubscribeMsgFactoriesTestParameters, SubscribeMsgFactoriesTest,
+    ::testing::Values(
+        std::make_tuple(std::vector<uint8_t>{0x0, 0x1, 0x2, 0x3, 0x4, 0x5}, 0x0001),
+        std::make_tuple(std::vector<uint8_t>{0x0, 0x0, 0x0, 0x0, 0x0, 0x5}, 0x0000),
+        std::make_tuple(std::vector<uint8_t>{255, 255, 255, 255, 255, 255}, 32767)
+    )
+);
+
+TEST_P(SubscribeMsgFactoriesTest, CONSTRUCTOR_CHECK) {
+    uint8_t length = 0x2;
+    auto params = GetParam();
+    const uint16_t id = std::get<1>(params);
+    simba::mw::temp::SubscribeHeader hdr{id};
+    EXPECT_EQ(id, hdr.GetServiceID());
+    EXPECT_EQ(length, hdr.GetLength());
+}
+
+TEST_P(SubscribeMsgFactoriesTest, SUBSCRIBE_MSG_FACTORIES_TEST) {
     simba::mw::temp::SubMsgFactory factory;
-    std::vector<uint8_t> payload = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5};
-    const uint16_t id = 0x0001;
+    auto params = GetParam();
+    std::vector<uint8_t> payload = std::get<0>(params);
+    const uint16_t id = std::get<1>(params);
     simba::mw::temp::SubscribeHeader hdr{id};
     const std::vector<uint8_t> data = factory.GetBuffer(
         std::make_shared<simba::mw::temp::SubscribeHeader>(hdr), std::move(payload));
