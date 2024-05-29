031813a8a709484b9afaf3c4c546f64482e29a82
Mateusz <matikrajek42@gmail.com>
2024-05-29 19:48:57 +0200

    fix_cpplint

--- core/common/test/benchmarks/crc_benchmark.cc
+++ core/common/test/benchmarks/crc_benchmark.cc
@@ -12,7 +12,7 @@
 
 #include "core/common/crc_16.h"
 
-static void CrcBenchmark_U8(benchmark::State& state) {
+static void CrcBenchmark_U8(benchmark::State& state) {  // NOLINT
     for (auto _ : state) {
         uint8_t val = 240;
         simba::core::CRC16::calculate(val);
@@ -20,7 +20,7 @@ static void CrcBenchmark_U8(benchmark::State& state) {
 }
 BENCHMARK(CrcBenchmark_U8);
 
-static void CrcBenchmark_U16(benchmark::State& state) {
+static void CrcBenchmark_U16(benchmark::State& state) {  // NOLINT
     for (auto _ : state) {
         uint16_t val = 300;
         simba::core::CRC16::calculate(val);
@@ -28,7 +28,7 @@ static void CrcBenchmark_U16(benchmark::State& state) {
 }
 BENCHMARK(CrcBenchmark_U16);
 
-static void CrcBenchmark_U32(benchmark::State& state) {
+static void CrcBenchmark_U32(benchmark::State& state) {  // NOLINT
     for (auto _ : state) {
         uint32_t val = 33000;
         simba::core::CRC16::calculate(val);
@@ -36,7 +36,7 @@ static void CrcBenchmark_U32(benchmark::State& state) {
 }
 BENCHMARK(CrcBenchmark_U32);
 
-static void CrcBenchmark_U64(benchmark::State& state) {
+static void CrcBenchmark_U64(benchmark::State& state) {  // NOLINT
     for (auto _ : state) {
         uint64_t val = 330000;
         simba::core::CRC16::calculate(val);
@@ -44,7 +44,7 @@ static void CrcBenchmark_U64(benchmark::State& state) {
 }
 BENCHMARK(CrcBenchmark_U64);
 
-static void CrcBenchmark_I8(benchmark::State& state) {
+static void CrcBenchmark_I8(benchmark::State& state) {  // NOLINT
     for (auto _ : state) {
         int8_t val = 240;
         simba::core::CRC16::calculate(val);
@@ -52,7 +52,7 @@ static void CrcBenchmark_I8(benchmark::State& state) {
 }
 BENCHMARK(CrcBenchmark_I8);
 
-static void CrcBenchmark_I16(benchmark::State& state) {
+static void CrcBenchmark_I16(benchmark::State& state) {  // NOLINT
     for (auto _ : state) {
         int16_t val = 300;
         simba::core::CRC16::calculate(val);
@@ -60,7 +60,7 @@ static void CrcBenchmark_I16(benchmark::State& state) {
 }
 BENCHMARK(CrcBenchmark_I16);
 
-static void CrcBenchmark_I32(benchmark::State& state) {
+static void CrcBenchmark_I32(benchmark::State& state) {  // NOLINT
     for (auto _ : state) {
         int32_t val = 33000;
         simba::core::CRC16::calculate(val);
@@ -68,7 +68,7 @@ static void CrcBenchmark_I32(benchmark::State& state) {
 }
 BENCHMARK(CrcBenchmark_I32);
 
-static void CrcBenchmark_I64(benchmark::State& state) {
+static void CrcBenchmark_I64(benchmark::State& state) {  // NOLINT
     for (auto _ : state) {
         int64_t val = 330000;
         simba::core::CRC16::calculate(val);
