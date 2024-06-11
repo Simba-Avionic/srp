d16ddbf16158db80df9633162dfa3f606c387296
Michal-Mankowski <m.mankowski2004@gmail.com>
2024-06-11 23:24:31 +0200

    temp controller tests done

--- mw/temp/tests/test_temp_controller.cc
+++ mw/temp/tests/test_temp_controller.cc
@@ -18,30 +18,48 @@
 class TestWrapper : public simba::mw::temp::TempController {
  public:
   simba::core::ErrorCode TestInit(uint16_t service_id, std::unique_ptr<MockSocket> socket) {
-      return simba::core::ErrorCode::kOk;
+      return Init(service_id, std::move(socket));
   }
   simba::core::ErrorCode TestSubscribe() {
     return this->Subscribe();
   }
+  simba::core::ErrorCode TestSetUp(simba::com::soc::RXCallback callback) {
+    return this->SetUp(callback);
+  }
 };
 
-class TestTempSub : public ::testing::TestWithParam<
-    uint16_t>{
-};
 
-INSTANTIATE_TEST_SUITE_P(TestTempSubParam, TestTempSub,
-::testing::Values(
-    0
-));
-
-TEST_P(TestTempSub, TEMP_SUBSCRIBE_TEST) {
+TEST(TEMP_CONTROLLER, TEMP_SUBSCRIBE_TEST) {
     TestWrapper wrapper{};
-    // uint16_t service_id = GetParam();
-    // auto mock_socket = std::make_unique<MockSocket>();
-    // EXPECT_CALL(*mock_socket, Transmit(::testing::_, ::testing::_, ::testing::_))
-    //   .WillOnce(::testing::Return(simba::core::ErrorCode::kOk));
-    // wrapper.TestInit(service_id, std::move(mock_socket));
-    // wrapper.TestSubscribe();
-    // EXPECT_EQ(wrapper.TestSubscribe(), simba::core::ErrorCode::kOk);
+    auto mock_socket = std::make_unique<MockSocket>();
+    EXPECT_CALL(*mock_socket, Transmit(::testing::_, ::testing::_, ::testing::_))
+      .WillOnce(::testing::Return(simba::core::ErrorCode::kOk))
+      .WillOnce(::testing::Return(simba::core::ErrorCode::kError));
+    wrapper.TestInit(0, std::move(mock_socket));
+    EXPECT_EQ(wrapper.TestSubscribe(), simba::core::ErrorCode::kOk);
+    EXPECT_EQ(wrapper.TestSubscribe(), simba::core::ErrorCode::kError);
+}
 
+
+TEST(TEMP_CONTROLLER, TEMP_SETUP_TEST) {
+  TestWrapper wrapper{};
+    auto mock_socket = std::make_unique<MockSocket>();
+    EXPECT_CALL(*mock_socket, Transmit(::testing::_, ::testing::_, ::testing::_))
+      .WillOnce(::testing::Return(simba::core::ErrorCode::kOk))
+      .WillOnce(::testing::Return(simba::core::ErrorCode::kError));
+    EXPECT_CALL(*mock_socket, Init(::testing::_))
+      .WillOnce(::testing::Return(simba::core::ErrorCode::kOk))
+      .WillOnce(::testing::Return(simba::core::ErrorCode::kOk))
+      .WillOnce(::testing::Return(simba::core::ErrorCode::kInitializeError));
+    EXPECT_CALL(*mock_socket, SetRXCallback(::testing::_))
+      .WillOnce(::testing::Return())
+      .WillOnce(::testing::Return());
+    EXPECT_CALL(*mock_socket, StartRXThread())
+      .WillOnce(::testing::Return());
+    wrapper.TestInit(0, std::move(mock_socket));
+    auto callback = [](const std::string& ip,
+      const std::uint16_t& port, const std::vector<std::uint8_t> data){};
+    EXPECT_EQ(wrapper.TestSetUp(callback), simba::core::ErrorCode::kOk);
+    EXPECT_EQ(wrapper.TestSetUp(callback), simba::core::ErrorCode::kError);
+    EXPECT_EQ(wrapper.TestSetUp(callback), simba::core::ErrorCode::kInitializeError);
 }
