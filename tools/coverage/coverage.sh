7c828f72b104aed9f91991802b0ba8e5147f4086
Mateusz <matikrajek42@gmail.com>
2024-07-19 17:11:59 +0200

    .

--- tools/coverage/coverage.sh
+++ tools/coverage/coverage.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Uruchomienie bazel coverage i ukrycie wyjścia
-bazel coverage --combined_report=lcov > /dev/null 2>&1
+bazel coverage -s \
+  --instrument_test_targets \
+  --experimental_cc_coverage \
+  --combined_report=lcov \
+  --coverage_report_generator=@bazel_tools//tools/test/CoverageOutputGenerator/java/com/google/devtools/coverageoutputgenerator:Main > /dev/null 2>&1
 
 # Uruchomienie bazel query i ukrycie wyjścia
 bazel query 'kind(cc_.*, tests(//...))' > /dev/null 2>&1
@@ -13,7 +17,7 @@ output=$(lcov --summary bazel-out/_coverage/_coverage_report.dat)
 line_coverage=$(echo "$output" | grep "lines......:" | awk '{print $2}' | sed 's/%//')
 
 # Wyodrębnienie % pokrycia funkcji za pomocą grep i awk
-function_coverage=$(echo "$output" | grep "functions..:" | awk '{print $2}' | sed 's/%//')
+echo "$output"
 
 # Wypisanie wyników
 echo "Pokrycie linii: $line_coverage%"
