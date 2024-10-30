37dfbcba24c6d2b9238937f3ef3d3b443fb928c6
Mateusz <matikrajek42@gmail.com>
2024-10-30 18:04:10 +0000

    fix action and improve performace cpplint

--- tools/cpplint/cpplint.sh
+++ tools/cpplint/cpplint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-cpp_files=$(find . -path "*libdoip/*" -prune -o -name "*.cc" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" | grep -vE "^\.\/libdoip\/" | grep -vE "^\.\/tools\/diag_app_frontend\/")
-total_errors=0
-for file in $cpp_files; do
-    errors=$(cpplint --filter=-build/include_subdir --extensions=cc,cpp,h,hpp --linelength=120 --recursive --exclude=*/libdoip/* --repository=. --root=src $file | wc -l)
-    if [[ $errors -gt 1 ]]; then
-    total_errors=$((total_errors + errors - 1))
-    fi
-done
-          
+# Znajdź wszystkie pliki do przetworzenia i zapisz je w zmiennej.
+cpp_files=$(find . -path "./libdoip" -prune -o -path "./tools/diag_app_frontend" -prune -o \( -name "*.cc" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -print)
+
+# Uruchom cpplint dla wszystkich plików jednocześnie i policz liczbę błędów.
+total_errors=$(cpplint --filter=-build/include_subdir --extensions=cc,cpp,h,hpp --linelength=120 $cpp_files 2>&1 | tee >(wc -l) | tail -n1)
+
+# Odejmij 1, aby dostosować wynik do warunku.
+total_errors=$((total_errors - 1))
+
+# Sprawdź liczbę błędów i wyświetl odpowiedni komunikat.
 if [[ $total_errors -gt 0 ]]; then
     echo "Too many errors ($total_errors), failing the build."
     exit 1
