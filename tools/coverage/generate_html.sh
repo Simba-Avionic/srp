9bda6f3a95e3b718249b0ecce1914a99c2a96b31
Mateusz <matikrajek42@gmail.com>
2024-11-13 14:49:42 +0000

    add coverage

--- tools/coverage/generate_html.sh
+++ tools/coverage/generate_html.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-bazel coverage --combined_report=lcov  `bazel query 'kind(cc_.*, tests(//...))'`
+
+bazel coverage //... -s \
+  --instrument_test_targets \
+  --experimental_cc_coverage \
+  --combined_report=lcov \
+#  --instrumentation_filter="//..."
+  --coverage_report_generator=@bazel_tools//tools/test/CoverageOutputGenerator/java/com/google/devtools/coverageoutputgenerator:Main > /dev/null 2>&1
+
+lcov --summary bazel-out/_coverage/_coverage_report.dat
+
+
 genhtml bazel-out/_coverage/_coverage_report.dat --output-directory coverage-report
