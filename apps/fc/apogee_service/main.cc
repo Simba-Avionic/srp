72293d12132ceee37b0812bdef22f99131e00afc
Wiktor-Laska <wiktorlaska06@gmail.com>
2026-05-11 12:34:52 +0200

    poprawineie ścieżek

--- apps/fc/apogee_service/main.cc
+++ apps/fc/apogee_service/main.cc
@@ -8,9 +8,9 @@
  * @copyright Copyright (c) 2026
  * 
  */
-#include "apps/fc/radio_service/apogee_app.hpp"
+#include "apps/fc/apogee_service/apogee_app.hpp"
 #include "ara/exec/adaptive_lifecycle.h"
 int main(int argc, char const *argv[]) {
-  return ara::exec::RunAdaptiveLifecycle<srp::apps::ApogeeApp>(argc,
+  return ara::exec::RunAdaptiveLifecycle<srp::apps::ApogeeService>(argc,
                                                                       argv);
 }
