dee4855714827a455f1acc65f8298cb9b87a6da9
Mateusz Krajewski <matikrajek42@gmail.com>
2026-05-17 16:02:34 +0000

    fix: fix cpplint

--- apps/fc/apogee_service/apogee_app.cpp
+++ apps/fc/apogee_service/apogee_app.cpp
@@ -6,9 +6,11 @@
  * @copyright Copyright (c) 2026
  */
 #include "apogee_app.hpp"
+
+#include <cmath>
+
 #include "ara/log/log.h"
 #include "core/common/condition.h"
-#include <cmath>
 
 namespace srp {
 namespace apps {
@@ -18,33 +20,29 @@ namespace {
   static constexpr auto kService_ipc_instance = "srp/apps/ApogeeDetectApp/ApogeeDetectService_ipc";
   static constexpr auto kService_udp_instance = "srp/apps/ApogeeDetectApp/ApogeeDetectService_udp";
 }
-ApogeeService::ApogeeService() : 
+
+ApogeeService::ApogeeService() :
     env_service_proxy{ara::core::InstanceSpecifier{kEnv_service_path}},
     env_service_handler{nullptr},
     service_ipc{ara::core::InstanceSpecifier{kService_ipc_instance}},
     service_udp{ara::core::InstanceSpecifier{kService_udp_instance}} {
 }
 
-
 int ApogeeService::Initialize(const std::map<ara::core::StringView, ara::core::StringView> parms) {
     ara::log::LogInfo() << "ApogeeService: Initializing...";
-    
-    this->service_ipc.StartOffer(); 
+
+    this->service_ipc.StartOffer();
     this->service_udp.StartOffer();
     this->SomeIpInit();
-    
+
     return 0;
 }
 
 void ApogeeService::SomeIpInit() {
-  
-  
   this->env_service_proxy.StartFindService([this](auto handler) {
     this->env_service_handler = handler;
-
     this->env_service_handler->newPressEvent.Subscribe(1, [this](const uint8_t status) {
       this->env_service_handler->newPressEvent.SetReceiveHandler([this] () {
-        
         auto res = this->env_service_handler->newPressEvent.GetNewSamples();
         if (res.HasValue()) {
             const double current_pressure = static_cast<double>(res.Value()) * kEncodedBarToHpa;
@@ -55,14 +53,13 @@ void ApogeeService::SomeIpInit() {
       });
     });
   });
-  //TODO odczytywanie velocity
+  // TODO(wlaska): odczytywanie velocity
 }
 
 void ApogeeService::EvaluateApogee() {
     if (apogee_detector_.isApogeeReached()) {
-        return; 
+        return;
     }
-
     double current_height = this->latest_height_.load();
     double current_velocity = this->latest_velocity_.load();
 
@@ -79,16 +76,16 @@ void ApogeeService::EvaluateApogee() {
 
 int ApogeeService::Run(const std::stop_token& token) {
   ara::log::LogInfo() << "ApogeeService: Running in event-driven mode.";
-  
+
   while (!token.stop_requested()) {
     core::condition::wait_for(std::chrono::milliseconds(1000), token);
   }
   this->service_ipc.StopOffer();
   this->service_udp.StopOffer();
   ara::log::LogInfo() << "ApogeeService: Stopped.";
-  
+
   return 0;
 }
 
-} // namespace apps
-} // namespace srp
+}  // namespace apps
+}  // namespace srp
