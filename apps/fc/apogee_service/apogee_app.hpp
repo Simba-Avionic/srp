e48e737de527c298ce6d2036715b073ee4922e56
Mateusz Krajewski <matikrajek42@gmail.com>
2026-05-17 16:02:34 +0000

    fix: fix cpplint

--- apps/fc/apogee_service/apogee_app.hpp
+++ apps/fc/apogee_service/apogee_app.hpp
@@ -5,11 +5,11 @@
  * @date 2026-04-25
  * @copyright Copyright (c) 2026
  */
-#ifndef APPS_FC_APOGEE_SERVICE_HPP_
-#define APPS_FC_APOGEE_SERVICE_HPP_
+#ifndef APPS_FC_APOGEE_SERVICE_APOGEE_APP_HPP_
+#define APPS_FC_APOGEE_SERVICE_APOGEE_APP_HPP_
 
 #include <atomic>
-#include <chrono>
+#include <chrono>  // NOLINT
 #include <memory>
 #include <map>
 
@@ -21,37 +21,36 @@
 constexpr double kBasePressure = 1013.25;
 constexpr double kEncodedBarToHpa = 10.0;
 
-namespace srp
-{
+namespace srp {
 namespace apps {
-        class ApogeeService : public ara::exec::AdaptiveApplication
-        {
-        private:
-            // zapytać co tam dokładnie czym jest
-            env::EnvAppProxy env_service_proxy;
-            std::shared_ptr<env::EnvAppHandler> env_service_handler;
-            apps::ApogeeDetectServiceSkeleton service_ipc;
-            apps::ApogeeDetectServiceSkeleton service_udp;
-            RealTimeApogee apogee_detector_{15, -0.5, 0.0}; 
-            
-            std::atomic<double> latest_height_{0.0};
-            std::atomic<double> latest_velocity_{0.0};
 
-            std::chrono::steady_clock::time_point last_imu_time_;
-            bool first_imu_measurement_ = true;
+class ApogeeService : public ara::exec::AdaptiveApplication {
+ private:
+    env::EnvAppProxy env_service_proxy;
+    std::shared_ptr<env::EnvAppHandler> env_service_handler;
+    apps::ApogeeDetectServiceSkeleton service_ipc;
+    apps::ApogeeDetectServiceSkeleton service_udp;
+    RealTimeApogee apogee_detector_{15, -0.5, 0.0};
 
-            void SomeIpInit();
-            void EvaluateApogee();
+    std::atomic<double> latest_height_{0.0};
+    std::atomic<double> latest_velocity_{0.0};
 
-        protected:
-            int Run(const std::stop_token &token) override;
-            int Initialize(const std::map<ara::core::StringView, ara::core::StringView> parms) override;
+    std::chrono::steady_clock::time_point last_imu_time_;
+    bool first_imu_measurement_ = true;
 
-        public:
-            ApogeeService();
-            ~ApogeeService() = default;
-        };
-} // namespace apps
-} // namespace srp
+    void SomeIpInit();
+    void EvaluateApogee();
 
-#endif // APPS_FC_APOGEE_SERVICE_HPP_
+ protected:
+    int Run(const std::stop_token &token) override;
+    int Initialize(const std::map<ara::core::StringView, ara::core::StringView> parms) override;
+
+ public:
+    ApogeeService();
+    ~ApogeeService() = default;
+};
+
+}  // namespace apps
+}  // namespace srp
+
+#endif  // APPS_FC_APOGEE_SERVICE_APOGEE_APP_HPP_
