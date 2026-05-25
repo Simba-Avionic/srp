62a8f5632fd04f7c1b8f2480293aa19b7c2ef1ff
Wiktor Laska <113641219+Wiktor-Laska@users.noreply.github.com>
2026-05-25 18:48:33 +0200

    Add apogee app (#366)
    * aplikacja apogee_app
    
    * pozmienianie ścieżek na prawidłowe
    
    * poprawineie ścieżek
    
    * fix: fix cpplint
    
    * fix apogee app
    
    * add rest of apogee app
    
    * reduce system usage by migrating to std::atomic
    
    * Add apogee to logger
    
    ---------
    
    Co-authored-by: pggit_oidc9352610 <s203495@student.pg.edu.pl>
    Co-authored-by: Mateusz Krajewski <matikrajek42@gmail.com>

--- apps/fc/logger_service/data_type.hpp
+++ apps/fc/logger_service/data_type.hpp
@@ -11,9 +11,8 @@
 #ifndef APPS_FC_LOGGER_SERVICE_DATA_TYPE_HPP_
 #define APPS_FC_LOGGER_SERVICE_DATA_TYPE_HPP_
 
+#include <atomic>
 #include <cstdint>
-#include <mutex>  // NOLINT
-#include <shared_mutex>
 #include <string>
 
 namespace srp {
@@ -22,32 +21,32 @@ namespace logger {
 class Data_t {
  private:
   using tempType = int16_t;
-  using bmeType = float;
-  using systemStatType = float;
 
-  std::shared_mutex mutex_;
-  tempType board_temp1_{0};
-  tempType board_temp2_{0};
-  tempType board_temp3_{0};
-  bmeType bme_temp_{0.0F};
-  bmeType bme_humidity_{0.0F};
-  bmeType bme_altitude_{0.0F};
-  systemStatType cpu_usage_{0.0F};
-  systemStatType mem_usage_{0.0F};
-  systemStatType disk_utilization_{0.0F};
+  std::atomic<tempType> board_temp1_{0};
+  std::atomic<tempType> board_temp2_{0};
+  std::atomic<tempType> board_temp3_{0};
+
+  std::atomic<float> bme_temp_{0.0f};
+  std::atomic<float> bme_humidity_{0.0f};
+  std::atomic<float> bme_altitude_{0.0f};
+
+  std::atomic<float> sys_cpu_usage_{0.0f};
+  std::atomic<float> sys_mem_usage_{0.0f};
+  std::atomic<float> sys_disk_utilization_{0.0f};
+
+  std::atomic<bool> apogee_detected_{false};
+  std::atomic<bool> main_parachute_detected_{false};
 
  public:
   std::string get_header();
   std::string to_string(const std::string& timestamp);
-  void SetBoardTemp1(const tempType& temp);
-  void SetBoardTemp2(const tempType& temp);
-  void SetBoardTemp3(const tempType& temp);
-  void SetBmeTemp(const bmeType& value);
-  void SetBmeHumidity(const bmeType& value);
-  void SetBmeAltitude(const bmeType& value);
-  void SetCpuUsage(const systemStatType& value);
-  void SetMemUsage(const systemStatType& value);
-  void SetDiskUtilization(const systemStatType& value);
+  void SetBoardTemp1(tempType temp);
+  void SetBoardTemp2(tempType temp);
+  void SetBoardTemp3(tempType temp);
+  void SetBmeData(const float temp, const float humidity, const float altitude);
+  void SetSystemUsage(const float cpu_usage, const float mem_usage, const float disk_usage);
+  void SetApogeeDetected(const bool apogee_detected);
+  void SetMainParachuteDetected(const bool main_parachute_detected);
 };
 
 }  // namespace logger
