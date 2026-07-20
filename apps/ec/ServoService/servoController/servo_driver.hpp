29b63c2f720df69273cde47520eaf64ae1f4a89e
Wiktor Muller <w.muller@onyxtms.com>
2026-07-20 17:24:55 +0200

    Converting degrees to servo ticks - Conversion function - Changes in servos configurations in jsons

--- apps/ec/ServoService/servoController/servo_driver.hpp
+++ apps/ec/ServoService/servoController/servo_driver.hpp
@@ -15,6 +15,7 @@
 #include <optional>
 #include <unordered_map>
 #include <mutex>  // NOLINT
+#include <algorithm>
 
 #include "apps/ec/ServoService/servoController/servo_config_reader.hpp"
 #include "mw/gpio_server/controller/gpio_controller.hpp"
@@ -28,10 +29,18 @@ namespace srp {
 namespace service {
 
 namespace {  // NOLINT
+  static constexpr auto kMin_angle = 0;
+  static constexpr auto kMax_angle = 180;
+  static constexpr uint16_t kMin_ticks = 102;
+  static constexpr uint16_t kMax_ticks = 512;
+  static constexpr auto kMinServoUs = 500;
+  static constexpr auto kMaxServoUs = 2500;
+  static constexpr auto kServoCycleUs = 20000;  // 50Hz
+  static constexpr auto kPcaResolution = 4096;
   static constexpr auto kDefault_mosfet_active_time_ms = 2000;
   static constexpr uint8_t kOpenState = 1U;
   static constexpr uint8_t kCloseState = 0U;
-}
+}  // namespace
 
 class ServoDriver {
  private:
@@ -57,6 +66,25 @@ class ServoDriver {
   ServoDriver():
       logger_(ara::log::LoggingMenager::GetInstance()->CreateLogger(
           "srdr", "", ara::log::LogLevel::kWarn)) {}
+  uint16_t AngleToServoTicks(const int angle) {
+    if (angle < kMin_angle || angle > kMax_angle) {
+        logger_.LogWarn() << "ServoController.AngleToServoTicks: Angle out of range ("
+            << kMin_angle << "-" << kMax_angle << "): " << angle << ". Clamping value.";
+    }
+    const int safe_angle = std::min(kMax_angle, std::max(kMin_angle, angle));
+
+    // Adding half of the max angle (kMax_angle/2) for integer rounding
+    const uint16_t usPulseTime = (safe_angle*(kMaxServoUs-kMinServoUs)+(kMax_angle/2))/kMax_angle+kMinServoUs;
+    const uint16_t calculatedTicks = usPulseTime*kPcaResolution/kServoCycleUs;
+
+    if (calculatedTicks < kMin_ticks || calculatedTicks > kMax_ticks) {
+      logger_.LogWarn() << "ServoController.AngleToServoTicks: Calculated value out of range ("
+            << kMin_ticks << "-" << kMax_ticks << "): " << calculatedTicks << ". Clamping value.";
+    }
+
+    const uint16_t baseTicks = std::min(kMax_ticks, std::max(kMin_ticks, calculatedTicks));
+    return baseTicks;
+  }
   bool SetServoPosition(const srp::service::ServoRuntimeConfig& cfg, const uint8_t state) {
     std::unique_lock<std::mutex> lock(operation_mtx);
     if (cfg.mosfet_id != 0) {
@@ -66,8 +94,8 @@ class ServoDriver {
       }
     }
 
-    const uint16_t target_position = ((state == kOpenState) ? cfg.on_pos : cfg.off_pos)
-                          * config.pca9685_XO_corelation;
+    const uint16_t target_angle = ((state == kOpenState) ? cfg.on_pos : cfg.off_pos);
+    const uint16_t target_position = AngleToServoTicks(target_angle) * config.pca9685_XO_corelation;
 
     logger_.LogDebug() << "ServoController.ExecuteServoMovement: setting actuator "
                        << "to PWM " << target_position;
@@ -78,7 +106,7 @@ class ServoDriver {
     return true;
   }
 
-  std::optional<uint8_t> ReadServoPosition(const uint8_t channel) {
+  std::optional<uint16_t> ReadServoPosition(const uint8_t channel) {
     std::unique_lock<std::mutex> lock(operation_mtx);
     return driver_.ReadChannelPosition(channel);
   }
